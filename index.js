0910e3b8cd6a05171423cb91101d13709a44cab5
mihirpatel1112 <mihir.patel@lucentinnovation.com>
2025-07-05 20:11:16 +0530

    Add GitHub Actions for ECS deploy

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  res.send('Welcome to the aws API');
+  res.send('Welcome to the aws API cicd');
 });
 
 app.get('/health', (req, res) => {
